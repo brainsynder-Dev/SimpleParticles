9c0a896b71d114226d41f7462b32dfcfed2a91f5
Thatsmusic99 <tmtherobotcat@gmail.com>
2018-09-08 07:54:11 +0100

    Added plugin instance

--- src/main/particles/brainsynder/SimpleParticles.java
+++ src/main/particles/brainsynder/SimpleParticles.java
@@ -4,4 +4,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class SimpleParticles extends JavaPlugin {
 
+    private static SimpleParticles instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+    }
+
+    public static SimpleParticles getInstance() {
+        return instance;
+    }
 }
