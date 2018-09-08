be74cf23ebb916aae1b1040faa0a113173fa9c20
Thatsmusic99 <tmtherobotcat@gmail.com>
2018-09-08 07:54:34 +0100

    Added config.yml

--- src/main/particles/brainsynder/SimpleParticles.java
+++ src/main/particles/brainsynder/SimpleParticles.java
@@ -1,17 +1,28 @@
 package particles.brainsynder;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import particles.brainsynder.config.Configuration;
 
 public class SimpleParticles extends JavaPlugin {
 
+    private Configuration config;
     private static SimpleParticles instance;
 
     @Override
     public void onEnable() {
         instance = this;
+        loadConfiguration();
+    }
+
+    private void loadConfiguration() {
+        config = new Configuration(this, getDataFolder().getName(), "config.yml");
     }
 
     public static SimpleParticles getInstance() {
         return instance;
+    }
+
+    public Configuration getConfiguration() {
+        return config;
     }
 }
---
+++ src/main/particles/brainsynder/config/Configuration.java
@@ -0,0 +1,28 @@
+package particles.brainsynder.config;
+
+import org.bukkit.Material;
+import org.bukkit.plugin.Plugin;
+import simple.brainsynder.files.FileMaker;
+
+import java.util.Arrays;
+
+public class Configuration extends FileMaker {
+    public Configuration(Plugin plugin, String directory, String fileName) {
+        super(plugin, directory, fileName);
+    }
+
+    public void loadDefaults() {
+        setDefault("join-item.enabled", true);
+        setDefault("join-item.can-not-drop", true);
+        setDefault("join-item.can-not-move", true);
+        setDefault("join-item.inventory-slot", 1);
+        setDefault("join-item.item.material", Material.BOOK.name());
+        setDefault("join-item.item.data", 0);
+        setDefault("join-item.item.display-name", "&eSimpleParticles &6&l>> &7Selector");
+        setDefault("join-item.item.lore", Arrays.asList("&eChange me", "&ein config.yml."));
+        setDefault("lores.particles", true);
+        setDefault("lores.shapes", true);
+        setDefault("website-link.enabled", true);
+        setDefault("website-link.account", "https://brainsynder.us/cape/save/0");
+    }
+}
