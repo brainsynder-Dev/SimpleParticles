ae46e38cccf13efc8b1726db996d6866c4ced4f2
Thatsmusic99 <tmtherobotcat@gmail.com>
2018-09-08 07:54:51 +0100

    Added easy way to create join item

---
+++ src/main/particles/brainsynder/config/JoinItem.java
@@ -0,0 +1,27 @@
+package particles.brainsynder.config;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import particles.brainsynder.SimpleParticles;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JoinItem extends ItemStack {
+
+    public JoinItem() {
+        Configuration config = SimpleParticles.getInstance().getConfiguration();
+        this.setType(Material.getMaterial(config.getString("join-item.item.material")));
+        setDurability((short) config.getInt("join-item.item.data")); // Another way of setting data for older versions
+        ItemMeta meta = getItemMeta();
+        meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', config.getString("join-item.item.display-name")));
+        List<String> lore = new ArrayList<>(); // To translate color codes
+        for (String s : config.getStringList("join-item.item.lore")) {
+            lore.add(ChatColor.translateAlternateColorCodes('&', s));
+        }
+        meta.setLore(lore);
+        setItemMeta(meta);
+    }
+}
